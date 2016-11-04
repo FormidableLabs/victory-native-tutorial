16d3ce02be0fe75426a1a67b6aaf8f46f72760b7
Lauren <lauren.eastridge@formidable.com>
2016-11-04 14:16:12 -0700

    base react native project

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Victory Native Tutorial
+
+### Running demo
+```sh
+$ npm install
+$ npm start
+```
+
+Then open `demo/ios/Demo.xcodeproj` and run the app. (Note: `npm start` must run _before_ you run the app).
+
+## Contributor Covenant Code of Conduct
+
+Please review our [Code of Conduct][code] before contributing.
+
+[code]: https://github.com/FormidableLabs/builder-victory-component/blob/master/CONTRIBUTING.md#contributor-covenant-code-of-conduct
---
+++ demo/index.android.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  ScrollView,
+  StyleSheet,
+  Text
+} from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 50,
+    paddingRight: 50,
+    paddingTop: 50
+  },
+  text: {
+    fontSize: 18,
+    fontFamily: "Menlo",
+    fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: 30
+  }
+});
+
+class Demo extends Component {
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.text}>{"Victory Tutorial"}</Text>
+      </ScrollView>
+    );
+  }
+}
+
+AppRegistry.registerComponent("Demo", () => Demo);
